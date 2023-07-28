20e3ea1291a09998b63eceb5cb8bc84d4d9052cc
KeremDilmen <krmdlmn03@gmail.com>
2023-07-28 17:20:34 +0300

    json trained model

--- img_classification/img_classification/image.py
+++ img_classification/img_classification/image.py
@@ -1,63 +1,94 @@
-import matplotlib.pyplot as plt
+from collections import OrderedDict
+import requests
+from io import BytesIO
+from PIL import Image
 import numpy as np
-import tensorflow as tf
 import os
-import pickle as pk
-
+import json
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers.experimental.preprocessing import Resizing
 
-current_directory = os.path.join(os.path.dirname(__file__))
 img_height = 180
 img_width = 180
-class_names = ['bathroom', 'bedroom', 'kitchen', 'living_room']
+
+current_directory = os.path.join(os.path.dirname(__file__))
+json_path = os.path.join(current_directory, "panoramas.json")
+with open(json_path, 'r') as f:
+  data = json.load(f)
+
+def list_to_ordered_set(input_list):
+    ordered_dict = OrderedDict.fromkeys(input_list)
+    ordered_set = list(ordered_dict.keys())
+    return ordered_set
+
+def load_and_preprocess_image(data, degrees_increment=30):
+    images = []
+    labels = []
+    label = data['label']
+    url = data['url']
+    response = requests.get(url)
+    image = Image.open(BytesIO(response.content))
+    image = image.resize((180, 180))  # Optional: Resize the image to a desired size
+    image_array = np.array(image)
+    # Determine the number of segments based on the degrees increment
+    segments = int(360 / degrees_increment)
+
+    # Calculate the width of each segment
+    segment_width = int(image_array.shape[1] / segments)
+
+    # Split the panoramic image into segments and save them
+    for i in range(segments):
+        start_col = i * segment_width
+        end_col = (i + 1) * segment_width
+        segment_image_array = image_array[:, start_col:end_col, :]
+        images.append(segment_image_array)
+        labels.append(label)
+    images = [tf.image.resize(segment, [img_height, img_width]) for segment in images]
+    return images, labels
 
 def train():
-  current_directory = os.path.join(os.path.dirname(__file__))
-  data_dir = os.path.join(current_directory, "rooms")
+    images = []
+    labels = []
+    for d in data:
+        label = d['label']
+        url = d['url']
+        image_list, label_list = load_and_preprocess_image(d)
+        images.extend(image_list) 
+        labels.extend(label_list) 
+        
+    label_set = list_to_ordered_set(labels)
+    print(label_set)
+    label_to_index = {label: idx for idx, label in enumerate(label_set)}
+    index_to_label = {idx: label for label, idx in label_to_index.items()}
 
-  batch_size = 3
-  
+    labels = [label_to_index[label] for label in labels]
 
-  train_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
+    images = tf.convert_to_tensor(images, dtype=tf.float32)
+    labels = tf.convert_to_tensor(labels, dtype=tf.int32)
 
-  val_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="validation",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
+    dataset = tf.data.Dataset.from_tensor_slices((images, labels))
 
-  class_names = train_ds.class_names
-  print(class_names)
+    train_size = int(0.8 * len(data))
+    train_dataset = dataset.take(train_size)
+    test_dataset = dataset.skip(train_size)
 
-  AUTOTUNE = tf.data.AUTOTUNE
+    batch_size = 10  # Adjust this as needed
 
-  train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-  val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    train_dataset = train_dataset.shuffle(buffer_size=train_size).batch(batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
+    num_classes = len(label_set)
 
-  num_classes = len(class_names)
-
-  data_augmentation = keras.Sequential(
-    [
-      layers.RandomFlip("horizontal",
-                        input_shape=(img_height,
-                                    img_width,
-                                    3)),
-      layers.RandomRotation(0.1),
-      layers.RandomZoom(0.1),
-    ]
-  )
-
-  model = Sequential([
+    data_augmentation = keras.Sequential(
+        [
+            layers.experimental.preprocessing.Resizing(img_height, img_width, interpolation="bilinear"),
+            layers.experimental.preprocessing.RandomFlip("horizontal"),
+            layers.experimental.preprocessing.RandomRotation(0.1),
+            layers.experimental.preprocessing.RandomZoom(0.1),
+        ]
+    )
+    model = Sequential([
     data_augmentation,
     layers.Rescaling(1./255),
     layers.Conv2D(16, 3, padding='same', activation='relu'),
@@ -70,63 +101,26 @@ def train():
     layers.Flatten(),
     layers.Dense(128, activation='relu'),
     layers.Dense(num_classes, name="outputs")
-  ])
+    ])
 
-  model.compile(optimizer='adam',
-                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    model.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
                 metrics=['accuracy'])
 
-  epochs = 50
-  history = model.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=epochs
-  )
+    model.fit(
+        train_dataset,
+        validation_data=test_dataset.batch(batch_size),  # Batch the test dataset
+        epochs=5
+    )
 
-  acc = history.history['accuracy']
-  val_acc = history.history['val_accuracy']
+    test_dataset = test_dataset.batch(batch_size)
+    test_loss, test_accuracy = model.evaluate(test_dataset)
+    print("Test accuracy:", test_accuracy)
 
-  loss = history.history['loss']
-  val_loss = history.history['val_loss']
+    return model
 
-  epochs_range = range(epochs)
 
-  plt.figure(figsize=(8, 8))
-  plt.subplot(1, 2, 1)
-  plt.plot(epochs_range, acc, label='Training Accuracy')
-  plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-  plt.legend(loc='lower right')
-  plt.title('Training and Validation Accuracy')
-
-  plt.subplot(1, 2, 2)
-  plt.plot(epochs_range, loss, label='Training Loss')
-  plt.plot(epochs_range, val_loss, label='Validation Loss')
-  plt.legend(loc='upper right')
-  plt.title('Training and Validation Loss')
-  plt.show()
-
-  return model
-
-# def test(url, name):
-#     path = tf.keras.utils.get_file(name, origin=url)
-#     img = tf.keras.utils.load_img(
-#     path, target_size=(img_height, img_width)
-#     )
-
-#     plt.imshow(img)
-#     plt.show()
-
-#     img_array = tf.keras.utils.img_to_array(img)
-#     img_array = tf.expand_dims(img_array, 0) # Create a batch
-
-#     predictions = model.predict(img_array)
-#     score = tf.nn.softmax(predictions[0])
-
-#     print(
-#     "This image most likely belongs to {} with a {:.2f} percent confidence."
-#     .format(class_names[np.argmax(score)], 100 * np.max(score))
-#     )
-
+### TESTING ###
 model_dir = os.path.join(current_directory, "saved_model")
 if not os.path.exists(model_dir):
   model = train()
@@ -134,12 +128,11 @@ if not os.path.exists(model_dir):
 else:
   model = tf.keras.models.load_model('saved_model')
 
-def test2(path):
-    img = img = tf.keras.utils.load_img(
+label_list = ['living room', 'balcony', 'bathroom', 'bedroom', 'entrance', 'kitchen', 'art gallery', 'Uncategorized', 'kids room']
+def test(path):
+    img = tf.keras.utils.load_img(
     path, target_size=(img_height, img_width)
     )
-    plt.imshow(img)
-    plt.show()
     img_array = tf.keras.utils.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0) # Create a batch
 
@@ -148,25 +141,11 @@ def test2(path):
 
     print(
     "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
+    .format(label_list[np.argmax(score)], 100 * np.max(score))
     )
 
-#test_1
-path = os.path.join(current_directory, "rooms/bedroom/bedroom_1.jpeg")
-test2(path)
+path = os.path.join(current_directory, "bedroom_test.jpeg")
+test(path)
 
-#test_2
-path = os.path.join(current_directory, "rooms/bathroom/bathroom_6.jpeg")
-test2(path)
-
-#test_3
-path = os.path.join(current_directory, "kitchen_test.jpeg")
-test2(path)
-
-#test_4
-path = os.path.join(current_directory, "rooms/living_room/living_room_8.jpeg")
-test2(path)
-
-#test_5
-path = os.path.join(current_directory, "living_test.jpeg")
-test2(path)
+path = os.path.join(current_directory, "bathroom_test.jpeg")
+test(path)
